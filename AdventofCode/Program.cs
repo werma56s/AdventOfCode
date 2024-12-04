93e308568ab6ee4ee876a9014c721b6551b2962a
Weronika Górska <weronika.oliwia.gorska@gmail.com>
2024-12-04 18:20:33 +0100

    Day1 part two

--- AdventofCode/Program.cs
+++ AdventofCode/Program.cs
@@ -2,4 +2,5 @@
 // Day1
 Day1HistorianHysteria day1HistorianHysteria = new Day1HistorianHysteria();
 //var(l1, l2) = day1HistorianHysteria.getDataToList();
-day1HistorianHysteria.GetDiverencesBetweenTheSmallestNumberOfLists();
+//day1HistorianHysteria.GetDiverencesBetweenTheSmallestNumberOfLists();
+day1HistorianHysteria.SimilarityScore();
